11a77d3cd837cc05ec51422a0561613fafe9e1a1
Josh Holt <holt.josh@gmail.com>
2012-07-18 14:39:21 -0300

    Update master

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,6 +12,7 @@ bash "Install ZSH" do
     git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
     cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
     sed -i 's/ZSH_THEME\=.*/ZSH_THEME\=\"fino-time\"/g' /home/vagrant/.zshrc
+    sed -i '1i\\ source /etc/profile' /home/vagrant/.zshrc
     echo 'source /home/vagrant/.bash_golang' >> /home/vagrant/.zshrc
     sudo chsh -s /bin/zsh vagrant
   EOC
